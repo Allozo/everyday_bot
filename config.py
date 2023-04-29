f854b90ecb727ed350910077a45766ac3e1e4541
allozo <allozo@yandex.ru>
2023-04-30 00:29:45 +0300

    Убрал ненужные проверки в получении переменных окружения

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from dotenv import load_dotenv
 
@@ -15,26 +14,8 @@ class Production:
     REDIS_HOST = os.environ.get('REDIS_HOST')
     REDIS_PORT = os.environ.get('REDIS_PORT')
 
-    if TYPE_CACHE is None:
-        sys.exit('Не указан TYPE_CACHE, который отвечает за выбор кэша')
-
-    if TYPE_CACHE == 'REDIS':
-        if REDIS_HOST is None or REDIS_PORT is None:
-            sys.exit(
-                'В качестве кэша выбран REDIS, но не переданы REDIS_HOST и REDIS_PORT для него'
-            )
-
 
 class Test:
     TYPE_CACHE = os.environ.get('TEST_TYPE_CACHE')
     REDIS_HOST = os.environ.get('TEST_REDIS_HOST')
     REDIS_PORT = os.environ.get('TEST_REDIS_PORT')
-
-    if TYPE_CACHE is None:
-        sys.exit('Не указан TEST_TYPE_CACHE, который отвечает за выбор кэша')
-
-    if TYPE_CACHE == 'REDIS':
-        if REDIS_HOST is None or REDIS_PORT is None:
-            sys.exit(
-                'В качестве кэша выбран REDIS, но не переданы REDIS_HOST и REDIS_PORT для него'
-            )
